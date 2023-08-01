243b6a0aab7a20f3ffe1108ff959cd9b81fc4276
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-08-01 21:15:34 +0600

    image tag use

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,28 @@
 "use client";
-import custom from "./custom.module.css";
-import other from "./other.module.css";
-import outside from "@/app/style/outside.module.css"
+import { useState } from "react";
+import image1 from "../../public/image2.jpg";
 
 import styles from "./page.module.css";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function Home() {
+  const [color, setColor] = useState("red");
   const router = useRouter();
   const navigate = (name) => {
     router.push(name);
   };
   return (
     <main className={styles.main}>
-      <h1 className={custom.main}>style and Css with NExt js</h1>
-      <h1 className={other.main}>This is from other</h1>
-      <h1 className={outside.main}>This is from other</h1>
+      <div className="flex gap-5">
+        <Image width={200} height={200} src={image1} alt="img1" />
+        <Image
+          src="https://images.pexels.com/photos/1172064/pexels-photo-1172064.jpeg?cs=srgb&dl=pexels-oleksandr-canary-islands-1172064.jpg&fm=jpg"
+          width={300}
+          height={200}
+          alt="P"
+        />
+      </div>
     </main>
   );
 }
